dea70f801317c0a1d2cbae40c040503267e83fe2
AlexanderKozhevin <info@alexander-kozhevin.com>
2018-08-06 15:18:53 +0300

    test : prettier

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from './App';test
+import registerServiceWorker from "./registerServiceWorker";
 
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "react-router-redux";
 
-import App from './containers/App';
-import configureStore from './configureStore';
-import history from './history';
+import App from "./containers/App";
+import configureStore from "./configureStore";
+import history from "./history";
 
 const initialState = {};
 const store = configureStore(initialState, history);
-
 
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <App />
     </ConnectedRouter>
-  </Provider>
+  </Provider>,
 
-    , document.getElementById('root'));
+  document.getElementById("root")
+);
 registerServiceWorker();
