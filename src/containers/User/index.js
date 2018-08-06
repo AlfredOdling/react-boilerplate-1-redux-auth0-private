0194f2e0c0a26ffe877bf135c0a206a5eb86eaaf
AlexanderKozhevin <info@alexander-kozhevin.com>
2018-08-06 15:20:00 +0300

    test : prettier

--- src/containers/User/index.js
+++ src/containers/User/index.js
@@ -1,47 +1,46 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component, PropTypes } from "react";
 
-import { connect } from 'react-redux'
-
-import * as actions from '../../actions/user'
+import { connect } from "react-redux";
+//test
+import * as actions from "../../actions/user";
 class User extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
 
     const { isAuthenticated } = this.props.auth;
-    if (!isAuthenticated()){
-      props.history.push('/login')
+    if (!isAuthenticated()) {
+      props.history.push("/login");
     }
   }
 
-  componentDidMount(){
-    console.log(this.props)
-    console.log(actions)
+  componentDidMount() {
+    console.log(this.props);
+    console.log(actions);
   }
 
   render() {
-
     return (
-        <div>
-          <h1>user page</h1>
-          <h2>{this.props.value}</h2>
-          <button onClick={()=>this.props.increment()}>increment</button>
-        </div>
-
-
-    )
+      <div>
+        <h1>user page</h1>
+        <h2>{this.props.value}</h2>
+        <button onClick={() => this.props.increment()}>increment</button>
+      </div>
+    );
   }
 }
-const mapDispatchToProps = (dispatch) => {
-    return {
-        increment: (url) => dispatch(actions.increment())
-    };
+const mapDispatchToProps = dispatch => {
+  return {
+    increment: url => dispatch(actions.increment())
+  };
 };
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   return {
     value: state.user
-  }
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(User)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(User);
